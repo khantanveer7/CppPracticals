957624d33ed5eceea31fb80fe6b54c9dcd6ae9e4
Khan Tanveer <khantanveer7977@gmail.com>
2021-01-26 17:52:46 +0530

    Matrix and Gcd Added

--- 18_MatrixAddtion.cpp
+++ 18_MatrixAddtion.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main()
 {
-    // check prev
+    int A[2][2], B[2][2], C[2][2];
+    cout << "Enter Value of A" << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+    cout << "Enter Value of B" << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> B[i][j];
+        }
+    }
+    cout << "Output" << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            C[i][j] = A[i][j] + B[i][j];
+            cout << C[i][j] << " ";
+        }
+    }
+
     return 0;
 }
